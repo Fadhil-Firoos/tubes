9b1157a734d6a35176f6d7970831109e67517d1c
fadhilfiroos@gmail.com <117986584+Fadhil-Firoos@users.noreply.github.com>
2023-04-18 10:29:04 +0700

    hallo

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from sys import exit
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(f"score {current_time}", False, (111,196,169))
+    score_surf = test_font.render(f"score {current_time}", False, ("#F0F0F0"))
     score_rect = score_surf.get_rect(center = (800, 255))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 850))
@@ -13,28 +14,35 @@ pygame.display.set_caption("Koceng Loncat")
 clock = pygame.time.Clock()
 game_active = False
 start_time = 0
+score = 0
 status = 1
 test_font = pygame.font.Font('asset/font/GAMERIA.ttf',75)
 
-game_over = test_font.render("Game Over", False, (111,196,169))
-game_over_rect = game_over.get_rect(center = (800, 255))
 
-sky_surface = pygame.image.load('asset/bg/1.png').convert()
-ground_surface = pygame.image.load('asset/bg/ground1.png').convert()
-logo_surf = pygame.image.load('asset/bg/logo/Koceng Loncat.png')
+game_over = test_font.render("Game Over", False, ("#F0F0F0"))
+game_over_rect = game_over.get_rect(center = (800, 200))
 
-obstacle_surf = pygame.image.load('asset/obstacle/1.png').convert_alpha()
+
+board_surf = pygame.image.load("asset/img/bg/board/end.png")
+board_rect = board_surf.get_rect(center = (800, 255))
+
+sky_surface = pygame.image.load('asset/img/bg/maps/1.png').convert()
+ground_surface = pygame.image.load('asset/img/bg/maps/ground1.png').convert()
+logo_surf = pygame.image.load('asset/img/bg/logo/Koceng_Loncat.png')
+
+obstacle_surf = pygame.image.load('asset/img/obstacle/1.png').convert_alpha()
 obstacle_rect = obstacle_surf.get_rect(bottomright = (1700, 680))
 
-koceng_surface = pygame.image.load('asset/koceng/run/5.png').convert_alpha()
+koceng_surface = pygame.image.load('asset/img/koceng/run/5.png').convert_alpha()
 koceng_rect = koceng_surface.get_rect(midbottom = (200, 680))
 koceng_gravity = 0
 
-koceng_stand = pygame.image.load('asset/intro/5.png').convert_alpha()
+koceng_stand = pygame.image.load('asset/img/intro/5.png').convert_alpha()
 koceng_stand_rect = koceng_stand.get_rect(center = (800, 580))
 
 logo_rect = logo_surf.get_rect(center = (800, 255))
 
+obstacle_timer = pygame.USEREVENT + 1
 
 while True:
     for event in pygame.event.get():
@@ -58,15 +66,15 @@ while True:
                 obstacle_rect.left = 1700
                 start_time = int(pygame.time.get_ticks() / 1000)
     
-    if game_active == False and status == 0: 
-            intro_message = test_font.render("Press space to run", False, ("#f9981f"))
-            intro_message_rect = game_over.get_rect(center = (800, 255))
-            
-    elif game_active == False and status == 1:
+
+    if game_active == False and status == 1:
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,654))
         screen.blit(logo_surf, logo_rect)   
         screen.blit(koceng_stand, koceng_stand_rect)
+        intro_message = test_font.render("Press space to run", False, ("#f9981f"))
+        intro_message_rect = intro_message.get_rect(center = (800, 400))
+        screen.blit(intro_message, intro_message_rect)
 
 
 
@@ -77,8 +85,7 @@ while True:
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,654))
 
-        if status != 0:
-            display_score()
+        score = display_score()
 
         # pygame.draw.ellipse( screen, 'brown', pygame.Rect(200, 580, 100, 100))
 
@@ -97,7 +104,14 @@ while True:
         #game over
         if obstacle_rect.colliderect(koceng_rect):
             game_active = False
+            score_message = test_font.render(f"Your score  {score}", False, ("#F0F0F0"))
+            score_message_rect = score_message.get_rect(center = (800, 300))
+            screen.blit(board_surf, board_rect)
             screen.blit(game_over, game_over_rect)
+            screen.blit(score_message, score_message_rect)
+            intro_message = test_font.render("Press space to run", False, ("#f9981f"))
+            intro_message_rect = intro_message.get_rect(center = (800, 580))
+            screen.blit(intro_message, intro_message_rect)
             status = 0
 
 
